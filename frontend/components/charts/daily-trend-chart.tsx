997f458a93f281cc67fa21573a1dd44ba4246c1f
ViMinhThang <huynhminhthang246@gmail.com>
2025-11-20 13:46:46 +0700

    1

--- frontend/components/charts/daily-trend-chart.tsx
+++ frontend/components/charts/daily-trend-chart.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { LineChart, Line, BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
+import { Card, CardContent, CardHeader, CardTitle, CardDescription } from '../../components/ui/card';
 import { DailyTrend } from '@/lib/db';
 import { format } from 'date-fns';
 
